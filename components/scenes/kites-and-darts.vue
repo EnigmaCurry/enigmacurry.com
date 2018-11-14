7f708099f52b6425d616a598eb7ec3853a5bc398
Ryan McGuire <ryan@enigmacurry.com>
2018-11-13 22:31:47 -0500

    Just something pretty

--- components/scenes/kites-and-darts.vue
+++ components/scenes/kites-and-darts.vue
@@ -4,13 +4,29 @@
       <g-light :hex="0xffffff" :intensity="1" :position="{ z: 10 }"/>
 
       <g-grid :divisions="10"/>
-      <!-- <g-grid :divisions="10" gridType="polar"/> -->
       <g-camera orthographic :zoomScale=2></g-camera>
-      <!-- <g-penrose-kite/> -->
-      <!-- <g-penrose-dart/> -->
-      <!-- <g-penrose-sun origin="top" :rotation="{z:0.5 * Math.PI}"/> -->
-      <!-- <g-penrose-star origin="top" :rotation="{z:Math.PI}"/> -->
-      <g-penrose-deuce origin="top" :rotation="{z:0.5 * Math.PI}"/>
+
+      <g-penrose-star />
+
+      <g-penrose-kite origin="top" :rotation="{z: Math.PI}"/>
+      <g-penrose-kite origin="top" :rotation="{z: Math.PI + (72*(Math.PI/180))}"/>
+      <g-penrose-kite origin="top" :rotation="{z: Math.PI + (144*(Math.PI/180))}"/>
+      <g-penrose-kite origin="top" :rotation="{z: Math.PI + (216*(Math.PI/180))}"/>
+      <g-penrose-kite origin="top" :rotation="{z: Math.PI + (288*(Math.PI/180))}"/>
+
     </scene>
   </g-renderer>
 </template>
+
+<script>
+export default {
+  data() {
+    let dartDimensions = this.$geometry.penrose.dartDimensions()
+    let kiteDimensions = this.$geometry.penrose.kiteDimensions()
+    return {
+      dartDimensions,
+      kiteDimensions
+    }
+  },  
+}
+</script>
