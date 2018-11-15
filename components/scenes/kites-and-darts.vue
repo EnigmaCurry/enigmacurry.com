9f72745e5c95dfced9c29ad2fca5b0d68a98d626
Ryan McGuire <ryan@enigmacurry.com>
2018-11-14 20:17:58 -0500

    Inifinite Sun Kings

--- components/scenes/kites-and-darts.vue
+++ components/scenes/kites-and-darts.vue
@@ -7,11 +7,18 @@
       <g-camera orthographic :zoomScale=3></g-camera>
 
       <g-penrose-sun origin="center"/>
+
       <g-penrose-queen :position="{y: kiteDimensions.height}" :rotation="{z: Math.PI}"/>
       <g-penrose-queen :position="{y: kiteDimensions.height}" :rotation="{z: 108 * (Math.PI/180)}" :worldRotation="72 * (Math.PI/180)" />
       <g-penrose-queen :position="{y: kiteDimensions.height}" :rotation="{z: 36 * (Math.PI/180)}" :worldRotation="144 * (Math.PI/180)" />
       <g-penrose-queen :position="{y: kiteDimensions.height}" :rotation="{z: 324 * (Math.PI/180)}" :worldRotation="216 * (Math.PI/180)" />
       <g-penrose-queen :position="{y: kiteDimensions.height}" :rotation="{z: 252 * (Math.PI/180)}" :worldRotation="288 * (Math.PI/180)" />
+
+      <g-penrose-king :position="{y: -2 * kiteDimensions.height}"/>
+      <g-penrose-king :position="{y: -2 * kiteDimensions.height}" :rotation="{z: 288 * (Math.PI/180)}" :worldRotation="72 * (Math.PI/180)"/>
+      <g-penrose-king :position="{y: -2 * kiteDimensions.height}" :rotation="{z: 216 * (Math.PI/180)}" :worldRotation="144 * (Math.PI/180)"/>
+      <g-penrose-king :position="{y: -2 * kiteDimensions.height}" :rotation="{z: 144 * (Math.PI/180)}" :worldRotation="216 * (Math.PI/180)"/>
+      <g-penrose-king :position="{y: -2 * kiteDimensions.height}" :rotation="{z: 72  * (Math.PI/180)}" :worldRotation="288 * (Math.PI/180)"/>
 
     </scene>
   </g-renderer>
