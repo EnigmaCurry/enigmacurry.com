66c7a71b5df47b8b82352f7d54b2e427098d110a
Ryan McGuire <ryan@enigmacurry.com>
2018-11-19 16:54:01 -0500

    Add iOS fullscreen support (?)

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
