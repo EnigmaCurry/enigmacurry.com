63a593b141c88da5e27a6782613cababcb14889a
Ryan McGuire <ryan@enigmacurry.com>
2018-12-05 09:50:34 -0500

    Tilings coloring

--- components/scenes/tilings.vue
+++ components/scenes/tilings.vue
@@ -19,33 +19,45 @@ export default {
     showGrid: {type: Boolean, default: false},
     zoom: {type: Number, default: 5},
     tileFrustrumSize: {type: Number, default: 10},
-    speedX: {type: Number, default: 0.07},
+    speedX: {type: Number, default: 0.01},
     speedY: {type: Number, default: 0.03},
     speedRotation: {type: Number, default: 0.002},
-    tileScale: {type: Number, default: 0.5}
   },
   data() {
     return {
       scene: new Three.Scene(),
       time: 0,
       tweenGroup: new TWEEN.Group(),
-      tileTypes: ["truncatedTriHexagonal", "triangular", "square", "hexagonal", "snubSquare", "elongatedTriangular",
-                  "truncatedSquare", "triHexagonal", "snubHexagonal1", "rhombiTriHexagonal",
-                  "truncatedHexagonal"],
+      backgroundTileTypes: ["truncatedTriHexagonal", "triangular", "square", "hexagonal", "snubSquare", "elongatedTriangular",
+                            "truncatedSquare", "triHexagonal", "snubHexagonal1", "rhombiTriHexagonal",
+                            "truncatedHexagonal"],
+      foregroundTileTypes: ["triangular", "square", "hexagonal", "snubSquare", "elongatedTriangular",
+                            "truncatedSquare", "triHexagonal", "snubHexagonal1", "rhombiTriHexagonal",
+                            "truncatedHexagonal", "truncatedTriHexagonal"],
     }
   },
   created() {    
     this.containerGroup = new Three.Group()
     this.scene.add(this.containerGroup)
-    this.materials = [
+    this.backgroundMaterials = [
       new Three.MeshLambertMaterial({color: 0xababab}),
+      new Three.MeshLambertMaterial({color: 0xfa3a3a}), 
       new Three.MeshLambertMaterial({color: 0xf0f0f0}),
-      new Three.MeshLambertMaterial({color: 0x343434}),
       new Three.MeshLambertMaterial({color: 'white'}),
     ]
+    this.foregroundMaterials = [
+      new Three.MeshLambertMaterial({color: 0xababab, transparent: true, opacity: 0.5}),
+      new Three.MeshLambertMaterial({color: 0x3a3afa, transparent: true, opacity: 0.5}), 
+      new Three.MeshLambertMaterial({color: 0xf0ffff, transparent: true, opacity: 0.5}),
+      new Three.MeshLambertMaterial({color: 'white', transparent: true, opacity: 0.5}),
+    ]
     
-    this.newTilingGroup()
-    setInterval(this.newTilingGroup, 20000)
+    this.newBackgroundTilingGroup()
+    this.newForegroundTilingGroup()
+    setInterval(() => {
+      this.newForegroundTilingGroup()
+      this.newBackgroundTilingGroup()
+    }, 20000)
     
     let light = new Three.PointLight(0xffffff, 2, 8)
     light.position.z = 5
@@ -59,20 +71,32 @@ export default {
   },
   methods: {
     animate() {
-      this.tilingGroup.pan(this.speedX, this.speedY)
+      this.backgroundTilingGroup.pan(this.speedX, this.speedY)
       this.containerGroup.rotation.z += this.speedRotation
       this.time += 0.01
     },
-    newTilingGroup() {
-      let tileType = this.tileTypes.shift()
-      this.tileTypes.push(tileType)
-      if(typeof(this.tilingGroup) != "undefined") {
-        this.containerGroup.remove(this.tilingGroup.group)
+    
+    newBackgroundTilingGroup() {
+      let tileType = this.backgroundTileTypes.shift()
+      this.backgroundTileTypes.push(tileType)
+      if(typeof(this.backgroundTilingGroup) != "undefined") {
+        this.containerGroup.remove(this.backgroundTilingGroup.group)
       }
       let frustrum = {left: -this.tileFrustrumSize, right: this.tileFrustrumSize, top:this.tileFrustrumSize, bottom:-this.tileFrustrumSize}
-      this.tilingGroup = new this.$tilings.TilingGroup({tileType, materials: this.materials, frustrum, showFrustrum: false})
-      this.containerGroup.add(this.tilingGroup.group)
+      this.backgroundTilingGroup = new this.$tilings.TilingGroup({tileType, materials: this.backgroundMaterials, frustrum, showFrustrum: false})
+      this.containerGroup.add(this.backgroundTilingGroup.group)
+    },
+    
+    newForegroundTilingGroup() {
+      let tileType = this.foregroundTileTypes.shift()
+      this.foregroundTileTypes.push(tileType)
+      if(typeof(this.foregroundTilingGroup) != "undefined") {
+        this.containerGroup.remove(this.foregroundTilingGroup.group)
+      }
+      let frustrum = {left: -this.tileFrustrumSize, right: this.tileFrustrumSize, top:this.tileFrustrumSize, bottom:-this.tileFrustrumSize}
+      this.foregroundTilingGroup = new this.$tilings.TilingGroup({tileType, materials: this.foregroundMaterials, frustrum, showFrustrum: false})
+      this.containerGroup.add(this.foregroundTilingGroup.group)
     }
-  }
+}
 }
 </script>
