9a31abdb952ef85c7d84de4867fdf2721e067f32
Ryan McGuire <ryan@enigmacurry.com>
2018-12-08 23:02:52 -0500

    web app config

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,9 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
-      { name: 'apple-mobile-web-app-capable', content: 'yes' }
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+      { name: 'apple-mobile-web-app-title', content: 'EnigmaCurry' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
