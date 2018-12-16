a48112de47c019d5f5258baaf1c4f6fa8ab2dffc
Ryan McGuire <ryan@enigmacurry.com>
2018-12-16 18:05:43 -0500

    finish sri-yantra gateways

--- components/scenes/sri-yantra.vue
+++ components/scenes/sri-yantra.vue
@@ -19,27 +19,40 @@ export default {
     animated: {type: Boolean, default: false},
     showGrid: {type: Boolean, default: false},
     showWires: {type: Boolean, default: false},
-    zoom: {type: Number, default: 1.5},
+    zoom: {type: Number, default: 2},
     innerRadius: {type: Number, default: 1}
   },
   data() {
     return {
       scene: new Three.Scene(),
       wireGeometry: new Three.Geometry(),
-      innerGeometry: new Three.Geometry(),
+      foregroundMeshes: [],
+      backgroundMeshes: [],
       wireMaterial: new Three.LineBasicMaterial({color: 'white'}),
       testMaterial: new Three.MeshBasicMaterial({color: "red"}),
-      innerMaterials: [
-        new Three.MeshBasicMaterial({color: "#ffffff"}), //0 - bindu
-        new Three.MeshBasicMaterial({color: "#fb0203"}), //1 - innermost triangle
-        new Three.MeshBasicMaterial({color: "#fff000"}), //2 - surrounding quadralaterals
-        new Three.MeshBasicMaterial({color: "#f84302"}), //3 - triangles
-        new Three.MeshBasicMaterial({color: "#00fff1"}), //4 
-        new Three.MeshBasicMaterial({color: "#fa0378"}), //5 - triangles
-        new Three.MeshBasicMaterial({color: "#01f37a"}), //6
-        new Three.MeshBasicMaterial({color: "#fe0000"}), //7 - triangles
-        new Three.MeshBasicMaterial({color: "#ba05d0"}), //8
-        new Three.MeshBasicMaterial({color: "#246d01"}), //9 - triangles        
+      foregroundMaterials: [
+        new Three.MeshBasicMaterial({color: "#fb0203"}), //0 - innermost triangle
+        new Three.MeshBasicMaterial({color: "#f84302"}), //1 - triangles
+        new Three.MeshBasicMaterial({color: "#fa0378"}), //2 - triangles
+        new Three.MeshBasicMaterial({color: "#fe0000"}), //3 - triangles
+        new Three.MeshBasicMaterial({color: "#246d01"}), //4 - triangles
+        new Three.MeshBasicMaterial({color: "#f91c53"}), //5 - petals 1
+        new Three.MeshBasicMaterial({color: "#fd615c"}), //6 - petals 2
+      ],
+      backgroundMaterials: [
+        new Three.MeshBasicMaterial({color: "#fff000"}), //0 - outside innermost triangle
+        new Three.MeshBasicMaterial({color: "#00feef"}), //1 - outside triangles
+        new Three.MeshBasicMaterial({color: "#01f37a"}), //2 - outside triangles
+        new Three.MeshBasicMaterial({color: "#ba05d0"}), //3 - outside triangles
+        new Three.MeshBasicMaterial({color: "#f1d006"}), //4 - inside circle
+        new Three.MeshBasicMaterial({color: "#f6f49d"}), //5 - inside petals
+        new Three.MeshBasicMaterial({color: "#fc17cb"}), //6 - inside gateway
+        new Three.MeshBasicMaterial({color: "#fefdfd"}), //7 - gateway threshold
+        new Three.MeshBasicMaterial({color: "#37f0f8"}), //7 - innermost gateway
+        new Three.MeshBasicMaterial({color: "#fefdfd"}), //8 - intergateway
+        new Three.MeshBasicMaterial({color: "#46f87e"}), //9 - second gateway
+        new Three.MeshBasicMaterial({color: "#fefdfd"}), //10 - intergateway
+        new Three.MeshBasicMaterial({color: "#dd00fe"}), //11 - third gateway
       ],
       center: new Three.Vector3()
     }
@@ -48,8 +61,14 @@ export default {
     /// Create wire mesh from triangles:
     this.createGeometry()
     let wires = new Three.LineSegments(new Three.EdgesGeometry(this.wireGeometry), this.wireMaterial)
-    let mesh = new Three.Mesh(this.innerGeometry, this.innerMaterials)
-    this.scene.add(mesh)
+    for(let fg=0; fg < this.foregroundMeshes.length; fg++) {
+      this.foregroundMeshes[fg].renderOrder = 2
+      this.scene.add(this.foregroundMeshes[fg])
+    }
+    for(let bg=0; bg < this.backgroundMeshes.length; bg++) {
+      this.backgroundMeshes[bg].renderOrder = -1 * bg
+      this.scene.add(this.backgroundMeshes[bg])
+    }
     this.scene.add(wires)
   },
   methods: {
@@ -257,69 +276,72 @@ export default {
       
       ///// Done with wire mesh
       ///// Start making full mesh
+      const trianglesGeometry = new Three.Geometry()
       
       const addMeshTriangle = (triTuple, level) => {
         let normal = new Three.Vector3(0, 0, -1)
-        let index = this.innerGeometry.vertices.length
-        this.innerGeometry.vertices.push(triTuple[0], triTuple[1], triTuple[2])
-        this.innerGeometry.faces.push(new Three.Face3(
-          index+2, index+1, index, normal, this.innerMaterials[level].color, level))
+        let index = trianglesGeometry.vertices.length
+        trianglesGeometry.vertices.push(triTuple[0], triTuple[1], triTuple[2])
+        trianglesGeometry.faces.push(new Three.Face3(
+          index+2, index+1, index, normal, this.foregroundMaterials[level].color, level))
         //TODO: might not need uv map?
-        //this.innerGeometry.faceVertexUvs[0].push([new Three.Vector2(),new Three.Vector2(),new Three.Vector2()])
+        //this.foregroundGeometry.faceVertexUvs[0].push([new Three.Vector2(),new Three.Vector2(),new Three.Vector2()])
       }
       
-      /// Add innermost triangle (material index 1)
-      addMeshTriangle([t6[0],t5t6[0],t5t6[1]], 1)
+      /// Add innermost triangle (material index 0)
+      addMeshTriangle([t6[0],t5t6[0],t5t6[1]], 0)
       
-      /// Add second layer of 9 triangles (material index 3)
-      addMeshTriangle([t5[0], t5t8[1], t5t8[0]], 3)
-      addMeshTriangle([t5t8[1], t6t7[1], t5t6[1]], 3)
-      addMeshTriangle([t5t6[1], t9[2], t5t9[1]], 3)
-      addMeshTriangle([t5t9[1], t5t8[3], t7t9[1]], 3)
-      addMeshTriangle([t7t9[0], t7t9[1], t9[0]], 3)
-      addMeshTriangle([t5t9[0], t7t9[0], t5t8[2]], 3)
-      addMeshTriangle([t5t6[0], t5t9[0], t9[1]], 3)
-      addMeshTriangle([t5t8[0], t5t6[0], t6t7[0]], 3)
+      /// Add second layer of 9 triangles (material index 1)
+      addMeshTriangle([t5[0], t5t8[1], t5t8[0]], 1)
+      addMeshTriangle([t5t8[1], t6t7[1], t5t6[1]], 1)
+      addMeshTriangle([t5t6[1], t9[2], t5t9[1]], 1)
+      addMeshTriangle([t5t9[1], t5t8[3], t7t9[1]], 1)
+      addMeshTriangle([t7t9[0], t7t9[1], t9[0]], 1)
+      addMeshTriangle([t5t9[0], t7t9[0], t5t8[2]], 1)
+      addMeshTriangle([t5t6[0], t5t9[0], t9[1]], 1)
+      addMeshTriangle([t5t8[0], t5t6[0], t6t7[0]], 1)
       
-      /// Add third layer of 10 triangles (material index 5)
-      addMeshTriangle([t2t7[1], t2t7[0], t7[0]], 5)
-      addMeshTriangle([t2t7[1], t2t3[1], t6t7[1]], 5)
-      addMeshTriangle([t6t7[1], t8[2], t7t8[1]], 5)
-      addMeshTriangle([t7t8[1], t7[2], t5t8[3]], 5)
-      addMeshTriangle([t5t8[3], t4t1[1], t1t8[1]], 5)
-      addMeshTriangle([t1t8[1], t8[0], t1t8[0]], 5)
-      addMeshTriangle([t5t8[2], t1t8[0], t4t1[0]], 5)
-      addMeshTriangle([t7t8[0], t5t8[2], t7[1]], 5)
-      addMeshTriangle([t6t7[0], t7t8[0], t8[1]], 5)
-      addMeshTriangle([t2t7[0], t6t7[0], t2t3[0]], 5)
+      /// Add third layer of 10 triangles (material index 2)
+      addMeshTriangle([t2t7[1], t2t7[0], t7[0]], 2)
+      addMeshTriangle([t2t7[1], t2t3[1], t6t7[1]], 2)
+      addMeshTriangle([t6t7[1], t8[2], t7t8[1]], 2)
+      addMeshTriangle([t7t8[1], t7[2], t5t8[3]], 2)
+      addMeshTriangle([t5t8[3], t4t1[1], t1t8[1]], 2)
+      addMeshTriangle([t1t8[1], t8[0], t1t8[0]], 2)
+      addMeshTriangle([t5t8[2], t1t8[0], t4t1[0]], 2)
+      addMeshTriangle([t7t8[0], t5t8[2], t7[1]], 2)
+      addMeshTriangle([t6t7[0], t7t8[0], t8[1]], 2)
+      addMeshTriangle([t2t7[0], t6t7[0], t2t3[0]], 2)
       
-      /// Add fourth layer of 10 triangles (material index 7)
-      addMeshTriangle([t3[0], t3t4[3], t3t4[2]], 7)
-      addMeshTriangle([t3t4[3], t6t1[1], t2t3[1]], 7)
-      addMeshTriangle([t2t3[1], t1t2[4], t3t4[1]], 7)
-      addMeshTriangle([t3t4[1], t1t2[5], t4t1[1]], 7)
-      addMeshTriangle([t4t1[1], t5t2[1], t3t4[5]], 7)
-      addMeshTriangle([t3t4[5], t4[0], t3t4[4]], 7)
-      addMeshTriangle([t4t1[0], t3t4[4], t5t2[0]], 7)
-      addMeshTriangle([t3t4[0], t4t1[0], t1t2[2]], 7)
-      addMeshTriangle([t2t3[0], t3t4[0], t1t2[1]], 7)
-      addMeshTriangle([t6t1[0], t3t4[2], t2t3[0]], 7)
+      /// Add fourth layer of 10 triangles (material index 3)
+      addMeshTriangle([t3[0], t3t4[3], t3t4[2]], 3)
+      addMeshTriangle([t3t4[3], t6t1[1], t2t3[1]], 3)
+      addMeshTriangle([t2t3[1], t1t2[4], t3t4[1]], 3)
+      addMeshTriangle([t3t4[1], t1t2[5], t4t1[1]], 3)
+      addMeshTriangle([t4t1[1], t5t2[1], t3t4[5]], 3)
+      addMeshTriangle([t3t4[5], t4[0], t3t4[4]], 3)
+      addMeshTriangle([t4t1[0], t3t4[4], t5t2[0]], 3)
+      addMeshTriangle([t3t4[0], t4t1[0], t1t2[2]], 3)
+      addMeshTriangle([t2t3[0], t3t4[0], t1t2[1]], 3)
+      addMeshTriangle([t6t1[0], t3t4[2], t2t3[0]], 3)
       
-      /// Add fifth layer of 14 triangles (material index 9)
-      addMeshTriangle([t1[2], t6t1[3], t6t1[2]], 9)
-      addMeshTriangle([t6t1[3], t6[2], t6t1[1]], 9)
-      addMeshTriangle([t6t1[1], t4[2], t1t2[4]], 9)
-      addMeshTriangle([t1t2[4], t2[1], t1t2[3]], 9)
-      addMeshTriangle([t1t2[3], t1[1], t1t2[5]], 9)
-      addMeshTriangle([t1t2[5], t3[2], t5t2[1]], 9)
-      addMeshTriangle([t5t2[1], t5[2], t5t2[3]], 9)
-      addMeshTriangle([t5t2[3], t2[2], t5t2[2]], 9)
-      addMeshTriangle([t5t2[0], t5t2[2], t5[1]], 9)
-      addMeshTriangle([t1t2[2], t5t2[0], t3[1]], 9)
-      addMeshTriangle([t1t2[2], t1[0], t1t2[0]], 9)
-      addMeshTriangle([t1t2[1], t1t2[0], t2[0]], 9)
-      addMeshTriangle([t6t1[0], t1t2[1], t4[1]], 9)
-      addMeshTriangle([t6t1[2], t6t1[0], t6[1]], 9)
+      /// Add fifth layer of 14 triangles (material index 4)
+      addMeshTriangle([t1[2], t6t1[3], t6t1[2]], 4)
+      addMeshTriangle([t6t1[3], t6[2], t6t1[1]], 4)
+      addMeshTriangle([t6t1[1], t4[2], t1t2[4]], 4)
+      addMeshTriangle([t1t2[4], t2[1], t1t2[3]], 4)
+      addMeshTriangle([t1t2[3], t1[1], t1t2[5]], 4)
+      addMeshTriangle([t1t2[5], t3[2], t5t2[1]], 4)
+      addMeshTriangle([t5t2[1], t5[2], t5t2[3]], 4)
+      addMeshTriangle([t5t2[3], t2[2], t5t2[2]], 4)
+      addMeshTriangle([t5t2[0], t5t2[2], t5[1]], 4)
+      addMeshTriangle([t1t2[2], t5t2[0], t3[1]], 4)
+      addMeshTriangle([t1t2[2], t1[0], t1t2[0]], 4)
+      addMeshTriangle([t1t2[1], t1t2[0], t2[0]], 4)
+      addMeshTriangle([t6t1[0], t1t2[1], t4[1]], 4)
+      addMeshTriangle([t6t1[2], t6t1[0], t6[1]], 4)
+      
+      this.foregroundMeshes.push(new Three.Mesh(trianglesGeometry, this.foregroundMaterials))
       
       /// Add Bindu
       this.marker(this.center, 'white', 0.01)
@@ -362,7 +384,7 @@ export default {
       for (let i=0; i < 8; i++) {
         let p = petal1.clone()
         p.rotation.z = i * (360/8) * (Math.PI/180)
-        this.scene.add(p)
+        this.foregroundMeshes.push(p)
       }
       
       /// Add Petals level 2
@@ -390,8 +412,70 @@ export default {
       for (let i=0; i < 16; i++) {
         let p = petal2.clone()
         p.rotation.z = i * (360/16) * (Math.PI/180)
-        this.scene.add(p)
+        this.foregroundMeshes.push(p)
       }
+
+      /// Add background layers
+      this.backgroundMeshes.push(new Three.Mesh(new Three.CircleGeometry(circle3Radius, 64), this.backgroundMaterials[5]))
+      
+      /// Add gateway
+      const squareGuide1 = new Three.PlaneGeometry(circle3Radius * 2, circle3Radius * 2)
+      const gwLevel = (level) => {
+        const levelIncrease = level * 0.02 * this.innerRadius
+        let array = []
+        array[0] = new Three.Vector3(squareGuide1.vertices[0].x - levelIncrease, squareGuide1.vertices[0].y + levelIncrease)
+        array[1] = new Three.Vector3(((circle3Radius * 2) / 3) - circle3Radius - levelIncrease, squareGuide1.vertices[0].y + levelIncrease)
+        array[2] = new Three.Vector3(array[1].x, t7[0].y - t2[2].y - levelIncrease)
+        array[3] = new Three.Vector3(array[1].x - 0.1 * this.innerRadius, array[2].y)
+        array[4] = new Three.Vector3(array[3].x, array[3].y + 0.1 * this.innerRadius + 2*levelIncrease)
+        array[5] = new Three.Vector3(-1 * array[4].x, array[4].y)
+        array[6] = new Three.Vector3(-1 * array[3].x, array[3].y)
+        array[7] = new Three.Vector3(-1 * array[2].x, array[2].y)
+        array[8] = new Three.Vector3(-1 * array[1].x, array[1].y)
+        array[9] = new Three.Vector3(squareGuide1.vertices[1].x + levelIncrease, squareGuide1.vertices[1].y + levelIncrease)
+        array[10] = new Three.Vector3(array[9].x, 0.5 * ((circle3Radius * 2) / 3) + levelIncrease)
+        array[11] = new Three.Vector3(array[2].y, array[10].y)
+        array[12] = new Three.Vector3(array[11].x, array[11].y + 0.1 * this.innerRadius)
+        array[13] = new Three.Vector3(array[11].x + 0.1 * this.innerRadius + 2*levelIncrease, array[12].y)
+        array[14] = new Three.Vector3(array[13].x, -1 * array[13].y)
+        array[15] = new Three.Vector3(array[12].x, -1 * array[12].y)
+        array[16] = new Three.Vector3(array[11].x, -1 * array[11].y)
+        array[17] = new Three.Vector3(array[10].x, -1 * array[10].y)
+        array[18] = new Three.Vector3(squareGuide1.vertices[3].x + levelIncrease, squareGuide1.vertices[3].y - levelIncrease)
+        array[19] = new Three.Vector3(array[8].x, -1 * array[8].y)
+        array[20] = new Three.Vector3(array[7].x, -1 * array[7].y)
+        array[21] = new Three.Vector3(array[6].x, -1 * array[6].y)
+        array[22] = new Three.Vector3(array[5].x, -1 * array[5].y)
+        array[23] = new Three.Vector3(array[4].x, -1 * array[4].y)
+        array[24] = new Three.Vector3(array[3].x, -1 * array[3].y)
+        array[25] = new Three.Vector3(array[2].x, -1 * array[2].y)
+        array[26] = new Three.Vector3(array[1].x, -1 * array[1].y)
+        array[27] = new Three.Vector3(squareGuide1.vertices[2].x - levelIncrease, squareGuide1.vertices[2].y - levelIncrease)
+        array[28] = new Three.Vector3(-1 * array[17].x, array[17].y)
+        array[29] = new Three.Vector3(-1 * array[16].x, array[16].y)
+        array[30] = new Three.Vector3(-1 * array[15].x, array[15].y)
+        array[31] = new Three.Vector3(-1 * array[14].x, array[14].y) 
+        array[32] = new Three.Vector3(-1 * array[13].x, array[13].y)
+        array[33] = new Three.Vector3(-1 * array[12].x, array[12].y)
+        array[34] = new Three.Vector3(-1 * array[11].x, array[11].y)
+        array[35] = new Three.Vector3(-1 * array[10].x, array[10].y)
+        array[36] = array[0]
+        return array
+      }
+      
+      const gateMesh = (points, material) => {
+        const gateShape = new Three.Shape()
+        gateShape.moveTo(points[0].x, points[0].y)
+        for(let i=1; i < points.length; i++) {
+          gateShape.lineTo(points[i].x, points[i].y)
+        }
+        const gateGeometry = new Three.ShapeGeometry(gateShape)
+        this.backgroundMeshes.push(new Three.Mesh(gateGeometry, material))
+      }
+      for (let level=-2; level < 5; level++) {
+        gateMesh(gwLevel(level), this.backgroundMaterials[8 + level])
+      }
+
 
     }
   }
