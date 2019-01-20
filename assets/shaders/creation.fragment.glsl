235ec124508d7e51b1c2a65bba3c0c14a575d4e2
Ryan McGuire <ryan@enigmacurry.com>
2019-01-20 14:24:39 -0500

    Creation tweak

--- assets/shaders/creation.fragment.glsl
+++ assets/shaders/creation.fragment.glsl
@@ -9,14 +9,16 @@ varying vec2 vUv;
 void main(void)
 {
   vec2 p = -1.0 + 2.0 * vUv;
+  p.y = 0.2 * (p.x / p.y);
+  p.x = 0.3 * (p.y / p.x);
   float l = length(p);
-  float t = iGlobalTime;
+  float t = tan(iGlobalTime / 28.);
 
   vec3 c;
   for(int i=0; i < 3; i++) {
-    t += .07;
-    vec2 a = p + p/l * (1.+cos(t)) * abs(sin(l*9.-t*2.));
-    c[i] = .01/length( fract(0.5+a) - 0.5);
+    t += .27;
+    vec2 a = 0.5 * (p/l * (33.+sin(t)) * abs(1. + atan(l*9.-t*2.)));
+    c[i] = 0.01/length( fract(0.75+a) - 0.5);
   }
   gl_FragColor = vec4(c/l, 0.);
 }
