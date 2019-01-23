d227ca125ec69c1e0b8a91e2551a9486bd332b5d
Ryan McGuire <ryan@enigmacurry.com>
2019-01-23 13:16:30 -0500

    pingpong playlist

--- assets/shaders/pingpongverse.fragment.glsl
+++ assets/shaders/pingpongverse.fragment.glsl
@@ -25,15 +25,13 @@ float remap(float original_value, float original_min, float original_max, float 
   return new_min + (((original_value - original_min) / (original_max - original_min)) * (new_max - new_min));
 }
 
-vec4 starnest(void)
+vec4 pingpong1(in float time, in float modt)
 {
 	//get coords and direction
-	vec2 uv=vUv-1.25;
+	vec2 uv=(vUv-1.35);
 	uv.y*=iResolution.y/iResolution.x;
 	vec3 dir=vec3(uv*zoom,22.);
-	float time=iGlobalTime*speed+.25;
 
-  float modt = abs(sin(time)/3.);
 	//mouse rotation
 	//float a1=.5+iMouse.x/iResolution.x*2.;
   float a1 = modt * uv.y * dir.x/12.;
@@ -74,6 +72,163 @@ vec4 starnest(void)
 	return vec4(v*.01,1.);
 }
 
+vec4 pingpong2(in float time, in float modt)
+{
+	//get coords and direction
+	vec2 uv=((vUv-1.35) * 2.) + 0.5;
+	uv.y*=iResolution.y/iResolution.x;
+	vec3 dir=vec3(uv*zoom,22.);
+
+	//mouse rotation
+	//float a1=.5+iMouse.x/iResolution.x*2.;
+  float a1 = modt * uv.y * dir.x/12.;
+	//float a2=.8+iMouse.y/iResolution.y*2.;
+  float a2 = modt * uv.x * dir.y/2.;
+	mat2 rot1=mat2(cos(a1),sin(a1/time*time),-tan(a1),acos(a1));
+	mat2 rot2=mat2(atan(a2*time),sin(a2),-cos(pow(a2,12.)) + 0.2,cos(pow(a2,9.)));
+	dir.xz*=rot1;
+	dir.xy*=rot2;
+	vec3 from=vec3(1.,.5,0.5);
+	from+=vec3(time*2.,time,-2.);
+	from.xz*=rot1 * rot2;
+	from.xy*=rot2;
+
+	//volumetric rendering
+	float s=0.1,fade=1.;
+	vec3 v=vec3(0.);
+	for (int r=0; r<volsteps; r++) {
+		vec3 p=from+s*dir*.5;
+		p = abs(vec3(tile)-mod(p,vec3(tile*2.))); // tiling fold
+		float pa,a=pa=0.;
+		for (int i=0; i<iterations; i++) {
+			p=abs(p)/dot(p,p)-formuparam; // the magic formula
+			a+=abs(length(p)-pa); // absolute sum of average change
+			pa=length(p);
+		}
+		float dm=max(0.,darkmatter-a*a*.001); //dark matter
+		a*=a*a; // add contrast
+		if (r>6) fade*=1.-dm; // dark matter, don't render near
+		//v+=vec3(dm,dm*.5,0.);
+		v+=fade;
+		v+=vec3(s,s*s,s*s*s*s)*a*brightness*fade; // coloring based on distance
+		fade*=distfading; // distance fading
+		s+=stepsize;
+	}
+  float q = sin(modt * 22.);
+	v=mix(vec3(length(v)),v,saturation * q); //color adjust
+	return vec4(v*.01,1.);
+}
+
+vec4 pingpong3(in float time, in float modt)
+{
+	//get coords and direction
+	vec2 uv=((vUv-0.135) * 2.) + 0.5;
+	uv.y*=iResolution.y/iResolution.x;
+	vec3 dir=vec3(uv*zoom,22.);
+
+	//mouse rotation
+	//float a1=.5+iMouse.x/iResolution.x*2.;
+  float a1 = modt * uv.y * dir.x/12.;
+	//float a2=.8+iMouse.y/iResolution.y*2.;
+  float a2 = modt * uv.x * dir.y/2.;
+	mat2 rot1=mat2(cos(a1),sin(a1/time*time),-tan(a1),acos(a1));
+	mat2 rot2=mat2(atan(a2*time),sin(a2),-cos(pow(a2,12.)) + 0.2,cos(pow(a2,9.)));
+	dir.xz*=rot1;
+	dir.xy*=rot2;
+	vec3 from=vec3(1.,.5,0.5);
+	from+=vec3(time*2.,time,-2.);
+	from.xz*=rot1 * rot2;
+	from.xy*=rot2;
+
+	//volumetric rendering
+	float s=0.1,fade=1.;
+	vec3 v=vec3(0.);
+	for (int r=0; r<volsteps; r++) {
+		vec3 p=from+s*dir*.5;
+		p = abs(vec3(tile)-mod(p,vec3(tile*2.))); // tiling fold
+		float pa,a=pa=0.;
+		for (int i=0; i<iterations; i++) {
+			p=abs(p)/dot(p,p)-formuparam; // the magic formula
+			a+=abs(length(p)-pa); // absolute sum of average change
+			pa=length(p);
+		}
+		float dm=max(0.,darkmatter-a*a*.001); //dark matter
+		a*=a*a; // add contrast
+		if (r>6) fade*=1.-dm; // dark matter, don't render near
+		//v+=vec3(dm,dm*.5,0.);
+		v+=fade;
+		v+=vec3(s,s*s,s*s*s*s)*a*brightness*fade; // coloring based on distance
+		fade*=distfading; // distance fading
+		s+=stepsize;
+	}
+  float q = sin(modt * 22.);
+	v=mix(vec3(length(v)),v,saturation * q); //color adjust
+	return vec4(v*.01,1.);
+}
+
+vec4 pingpong4(in float time, in float modt)
+{
+	//get coords and direction
+	vec2 uv=((vUv-1.) * 1.);
+	uv.y*=iResolution.y/iResolution.x;
+	vec3 dir=vec3(uv*zoom,22.);
+
+	//mouse rotation
+	//float a1=.5+iMouse.x/iResolution.x*2.;
+  float a1 = modt * uv.y * dir.x/12.;
+	//float a2=.8+iMouse.y/iResolution.y*2.;
+  float a2 = modt * uv.x * dir.y/2.;
+	mat2 rot1=mat2(cos(a1),sin(a1/time*time),-tan(a1),acos(a1));
+	mat2 rot2=mat2(atan(a2*time),sin(a2),-cos(pow(a2,12.)) + 0.2,cos(pow(a2,9.)));
+	dir.xz*=rot1;
+	dir.xy*=rot2 * rot1;
+	vec3 from=vec3(1.,.5,0.5);
+	from+=vec3(time*2.,time,-2.);
+	from.xz*=rot1 * rot2;
+	from.xy*=rot2 * rot2 * rot2 * rot2;
+	from.xy*=rot2 * rot2 * rot2 * rot2;
+	from.xy*=rot2 * rot2 * rot2 * rot2;
+	from.xy*=rot2 * rot2 * rot2 * rot2;
+
+	//volumetric rendering
+	float s=0.1,fade=1.;
+	vec3 v=vec3(0.);
+	for (int r=0; r<volsteps; r++) {
+		vec3 p=from+s*dir*.5;
+		p = abs(vec3(tile)-mod(p,vec3(tile*2.))); // tiling fold
+		float pa,a=pa=0.;
+		for (int i=0; i<iterations; i++) {
+			p=abs(p)/dot(p,p)-formuparam; // the magic formula
+			a+=abs(length(p)-pa); // absolute sum of average change
+			pa=length(p);
+		}
+		float dm=max(0.,darkmatter-a*a*.001); //dark matter
+		a*=a*a; // add contrast
+		if (r>6) fade*=1.-dm; // dark matter, don't render near
+		//v+=vec3(dm,dm*.5,0.);
+		v+=fade;
+		v+=vec3(s,s*s,s*s*s*s)*a*brightness*fade; // coloring based on distance
+		fade*=distfading; // distance fading
+		s+=stepsize;
+	}
+  float q = sin(modt * 22.);
+	v=mix(vec3(length(v)),v,saturation * q); //color adjust
+	return vec4(v*.01,1.);
+}
+
+
 void main(void) {
-  gl_FragColor = starnest();
+	float time=iGlobalTime*speed;
+  float modt = abs(sin(time)/3.);
+
+  float framemod = mod(iGlobalTime, 420.);
+  if(framemod < 105.) {
+    gl_FragColor = pingpong1(time, modt);
+  } else if (framemod < 210.) {
+    gl_FragColor = pingpong2(time, modt);
+  } else if (framemod < 315.) {
+    gl_FragColor = pingpong3(time, modt);
+  } else if (framemod < 420.) {
+    gl_FragColor = pingpong4(time, modt);
+  }
 }
