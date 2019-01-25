acd6b463fe08177d33f8a114b481a890c4ba9544
Ryan McGuire <ryan@enigmacurry.com>
2019-01-25 11:10:00 -0500

    more zoom

--- assets/shaders/mandelbrot.fragment.glsl
+++ assets/shaders/mandelbrot.fragment.glsl
@@ -26,9 +26,9 @@ vec4 mandelbrot(vec4 area) {
                 acos(iGlobalTime / p.x) * 0.15 * p.y,
                 1.0);
   } else {
-    return vec4(col * 0.00001 + cos(iGlobalTime /22. * iter),
-                col * 0.00002 + cos(iGlobalTime /13. * iter),
-                col * 0.00005 + tan(iGlobalTime / 23. + iter ),
+    return vec4(col * 0.00001 + cos(iGlobalTime /22. * iter) + 0.15 * p.x,
+                col * 0.00002 + cos(iGlobalTime /13. * iter) + 0.15 * p.x,
+                col * 0.00005 + tan(iGlobalTime / 23. + iter ) + 0.15 * p.x,
                 1.0);
   }
 }
@@ -38,10 +38,10 @@ void main(void)
   //vec3 col = 0.5 * 0.5*cos(iGlobalTime + vUv.xyx + vec3(0,2,4));
   //gl_FragColor = vec4(col, 1.0);
 
-  float tmod = 48.0;
+  float tmod = 148.0;
   vec2 center = vec2(abs(sin(iGlobalTime / tmod)) * 0.01 - 0.46994, abs(sin(iGlobalTime / tmod)) * 0.1 - 0.641);
-  float s = 12.11595464;
-  float zoom = sin(iGlobalTime / 8.) * 7. + s;
+  float s = 22.11595464;
+  float zoom = sin(iGlobalTime / 8.) * 14. + s;
   zoom *= 0.15 * zoom;
   vec4 area = vec4(-zoom+center.x,-zoom+center.y,zoom+center.x,zoom+center.y);
   gl_FragColor = mandelbrot(area);
