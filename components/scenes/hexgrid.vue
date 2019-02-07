9fec2a59800e9495ee9a07a236f6570ff6d30d26
Ryan McGuire <ryan@enigmacurry.com>
2019-02-06 22:49:40 -0500

    hex grid

--- components/scenes/hexgrid.vue
+++ components/scenes/hexgrid.vue
@@ -10,6 +10,24 @@ import * as Three from 'three'
 import * as TWEEN from '@tweenjs/tween.js'
 import {shuffle} from 'underscore'
 import BackgroundImage from '~/components/BackgroundImage.vue'
+import Visibility from 'visibilityjs'
+
+function *spiralGenerator(generations, origin, direction) {
+  let order=0, n=0, d=direction
+  for (let g=0; g < generations; g++) {
+    if (order===0 || order===1 || n % order === 0) {
+      order += 1
+      n = 1
+      if (d > 0) {
+        d = (d - 1) % 6
+      } else {
+        d = 5
+      }
+    }
+    n += 1
+    yield d
+  }
+}
 
 export default {
   mixins: [BackgroundImage],
@@ -17,20 +35,25 @@ export default {
   props: {
     animated: {type: Boolean, default: false},
     showGrid: {type: Boolean, default: false},
-    zoom: {type: Number, default: 200},
+    zoom: {type: Number, default: 740},
     hexSize: {type: Number, default: 10},
-    hexBorder: {type: Number, default: 0.1},
-    generations: {type: Number, default: 6},
+    hexBorder: {type: Number, default: 0.01},
+    generations: {type: Number, default: 666},
   },
   data() {
     return {
       scene: new Three.Scene(),
+      generation: 0,
+      spirals: [],
+      origins: [],
       hexGeometry: new Three.CircleGeometry((1 - this.hexBorder) * this.hexSize, 6),
       hexMaterials: [
-        new Three.MeshBasicMaterial({color: 0xffffff}),
+        new Three.MeshBasicMaterial({color: 0x666666}),
+        new Three.MeshBasicMaterial({color: 0xeeeeee}),
+        new Three.MeshBasicMaterial({color: 0xdddddd}),
         new Three.MeshBasicMaterial({color: 0xcccccc}),
-        new Three.MeshBasicMaterial({color: 0x444444}),
-        new Three.MeshBasicMaterial({color: 0x222222}),
+        new Three.MeshBasicMaterial({color: 0xbbbbbb}),
+        new Three.MeshBasicMaterial({color: 0xaaaaaa})
       ],
       hexLayout: new this.$hexagons.Layout(this.$hexagons.Layout.flat,
                                            new Three.Vector2(this.hexSize, this.hexSize),
@@ -39,31 +62,28 @@ export default {
     }
   },
   mounted() {
-    const origin = new this.$hexagons.Hex(0,0,0)
-    this.newHexMesh(origin)
-    const addGeneration = (cells, generations) => {
-      if (generations < 1) {
-        return
-      }
-      const neighbors = []
-      for(let c=0; c < cells.length; c++) {
-        const o = cells[c]
-        for(let d=0; d < 6; d++){
-          const m = this.getNeighborMesh(o, d, generations)
-          if (m.isNew === true) {
-            neighbors.push(m.neighbor)
-          }
-        }
-      }
-      for(let n=0; n < neighbors.length; n++) {
-        if (generations > 0) {
-          addGeneration(neighbors, generations-1)
-        }
-      }
+    let origin = new this.$hexagons.Hex(0,0,0)
+    this.newHexMesh(origin)    
+    for (let d=0; d < 6; d++) {
+      this.spirals.push(spiralGenerator(this.generations, origin, d))
+      this.origins.push(origin)
     }
-    addGeneration([origin], this.generations)
+    this.nextGeneration()
+    Visibility.every(100, () => {
+      this.nextGeneration()
+    })
   },
   methods: {
+    nextGeneration() {
+      if (this.generation < this.generations) {
+        for (let s=0; s < this.spirals.length; s++) {
+          const spiral = this.spirals[s]
+          const nHex = this.origins[s] = this.origins[s].neighbor(spiral.next().value)
+          this.newHexMesh(nHex, s)
+        }
+        this.generation += 1
+      }
+    },
     newHexMesh(hex, matIndex=0) {
       const mesh = new Three.Mesh(this.hexGeometry, this.hexMaterials[matIndex % this.hexMaterials.length])
       const px = this.hexLayout.hexToPixel(hex)
