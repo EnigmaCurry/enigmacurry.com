a502e453ba30243b4398b733c9ae5192d189cffe
Ryan McGuire <ryan@enigmacurry.com>
2019-02-10 19:19:50 -0500

    vertex shader mod

--- assets/shaders/hexshader.vertex.glsl
+++ assets/shaders/hexshader.vertex.glsl
@@ -1,14 +1,10 @@
 uniform float iTime;
-uniform bool iKaleidoscope;
 varying vec2 vUv;
 void main()
 {
   vUv = uv;
-  float i = 0.;
-  if(iKaleidoscope) {
-    i = smoothstep(-1., 1., sin(iTime/9.)) / 12.;
-  }
+  float i = smoothstep(-1., 1., sin(iTime/9.)) / 12.;
   vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-  gl_Position = (projectionMatrix * mvPosition + i) * sqrt(i) * 2.;
+  gl_Position = (projectionMatrix * mvPosition + i) * sqrt(i) * 222. + (sin(iTime/vUv.y/vUv.x/22.) * 20.) + 20.;
 }
 
