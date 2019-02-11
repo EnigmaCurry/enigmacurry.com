ddbbf650874213918ed393383868228574caf041
Ryan McGuire <ryan@enigmacurry.com>
2019-02-10 19:00:47 -0500

    colormod

--- assets/shaders/hexshader.fragment.glsl
+++ assets/shaders/hexshader.fragment.glsl
@@ -11,8 +11,9 @@ void main(void)
 {
   float g = float(iGeneration);
   float c = float(iCreation);
-  float t = (iTime/(float(iCreation) * distance(vUv.x,vUv.y))) + float(iCreation) * 20.;
-  float i = smoothstep(-1., 1., sin(t)) * dot(c*vUv.x,g*vUv.y)/2222.;
-  vec3 color = vec3(i + iColor.r * vUv.x,i + iColor.g * vUv.y, i + iColor.b * sqrt(sin(i))+vUv.x*sin(t));
+  float v = max(vUv.x, vUv.y);
+  float t = (iTime/(float(iCreation))*pow(v,3.)) + float(iCreation) * 20.;
+  float i = smoothstep(-1., 1., sin(t));
+  vec3 color = vec3(i + iColor.r * v,i + iColor.g * v, i + iColor.b * sqrt(sin(i))+v*sin(t));
   gl_FragColor = vec4(color, iOpacity) * 0.5;
 }
