7ed392d252cc72a073ca75b285714b131365c43f
Ryan McGuire <ryan@enigmacurry.com>
2019-02-11 18:50:53 -0500

    hexshader

--- assets/shaders/hexshader2.fragment.glsl
+++ assets/shaders/hexshader2.fragment.glsl
@@ -5,7 +5,23 @@ uniform float iCreatedTime;
 uniform int iGeneration;
 uniform int iCreation;
 
+#define PI 3.14159
+
 varying vec2 vUv;
+
+float plateaus(float t, float transition, float plateau) {
+  float total = plateau + transition + plateau + transition;
+  float m = mod(t, total);
+  if (m < plateau) {
+    return 1.;
+  } else if (m < (plateau + transition)) {
+    return smoothstep(-1., 1., sin((t-plateau) / (transition/PI) + PI/2.));
+  } else if (m < (plateau + transition + plateau)) {
+    return 0.;
+  } else {
+    return smoothstep(-1., 1., sin((t-plateau-transition-plateau) / (transition/PI) - PI/2.));
+  }
+}
 
 void main(void)
 {
@@ -13,9 +29,11 @@ void main(void)
   float c = float(iCreation);
   float v = vUv.x * 3.;
   float t = pow(fract(iTime/(float(iCreation))*pow(v,333.)) + float(iCreation) * 20., 0.5);
-  t += sin((distance(g,c) / vUv.y) / 222.);
+  t += dot(c,v) / (vUv.x/vUv.y);
   float i = smoothstep(-1., 1., sin(t*22.));
   vec3 color = vec3(i + (iColor.r/t),i + (iColor.g/v) + sin(t), v + tan(iColor.b * v));
-  float a = smoothstep(-1., 1., sin(t*sqrt(g)/22.)) + 0.2;
-  gl_FragColor = vec4(color, a);
+  float a = smoothstep(-1., 1., sin(t*(sqrt(g)/22.))) + 0.2;
+
+  float p = plateaus(iTime, 4., 2.);
+  gl_FragColor = vec4((1./p) * iColor.r, iColor.g, p * iColor.b, a);
 }
