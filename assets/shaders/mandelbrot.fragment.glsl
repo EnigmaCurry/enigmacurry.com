8ea89633f4541b8ef36cf139c680bfbadc1ac380
Ryan McGuire <ryan@enigmacurry.com>
2019-02-12 12:09:37 -0500

    mandelbrot colormod mixing

--- assets/shaders/mandelbrot.fragment.glsl
+++ assets/shaders/mandelbrot.fragment.glsl
@@ -5,21 +5,20 @@ uniform float zoom;
 uniform float colt;
 
 #define iterLimit 256.
+#define black vec4(0.,0.,0.,1.)
 
 varying vec2 vUv;
+
 
 vec4 colmod1 (float iter, float t) {
   float col = iter / iterLimit;
   vec2 p = vUv - 0.5;
   if (col > 0.99) {
-    return vec4(cos(t / p.x) * 0.15 * p.y,
-                sin(t / p.x) * 0.15 * p.y,
-                acos(t / p.x) * 0.15 * p.y,
-                1.0);
+    return black;
   } else {
-    return vec4(col * 0.00001 + cos(t /22. * iter) + 0.15 * p.x,
-                col * 0.00002 + cos(t /13. * iter) + 0.15 * p.x,
-                col * 0.00005 + tan(t / 23. + iter ) + 0.15 * p.x,
+    return vec4(col * 0.00002 + cos(t /45. * iter) + 0.15 * p.x,
+                col * 0.00001 + cos(t /111. * iter) + 0.15 * p.x,
+                col * 0.00005 + tan(t / 555. + iter ) + 0.15 * p.x,
                 1.0);
   }
 }
@@ -28,14 +27,11 @@ vec4 colmod2 (float iter, float t) {
   float col = iter / iterLimit;
   vec2 p = vUv - 0.5;
   if (col > 0.99) {
-    return vec4(cos(t / p.x) * 0.15 * p.y,
-                sin(t / p.x) * 0.15 * p.y,
-                acos(t / p.x) * 0.15 * p.y,
-                1.0);
+    return black;
   } else {
-    return vec4(col * 0.00001 + cos(t /33. * iter) + 0.15 * p.x,
-                col * 0.00002 + cos(t /44. * iter) + 0.15 * p.x,
-                col * 0.00005 + tan(t / 55. + iter ) + 0.15 * p.x,
+    return vec4(col * 0.00002 + cos(t /33. * iter) + 0.15 * p.x,
+                col * 0.00003 + cos(t /444. * iter) + 0.15 * p.x,
+                col * 0.00005 + tan(t / 555. + iter ) + 0.15 * p.x,
                 1.0);
   }
 }
@@ -44,18 +40,14 @@ vec4 colmod3 (float iter, float t) {
   float col = iter / iterLimit;
   vec2 p = vUv - 0.5;
   if (col > 0.99) {
-    return vec4(cos(t / p.x) * 0.15 * p.y,
-                sin(t / p.x) * 0.15 * p.y,
-                acos(t / p.x) * 0.15 * p.y,
-                1.0);
+    return black;
   } else {
-    return vec4(col * 0.00001 + cos(t /33. * iter) + 0.15 * p.x,
-                col * 0.00002 + cos(t /44. * iter) + 0.15 * p.x,
-                col * 0.00005 + tan(t / 55. + iter ) + 0.15 * p.x,
+    return vec4(col * 0.00002 + cos(t /2222. * iter) + 0.15 * p.x,
+                col * 0.00001 + cos(t /1333. * iter) + 0.15 * p.x,
+                col * 0.00005 + tan(t /884. + iter ) + 0.15 * p.x,
                 1.0);
   }
 }
-
 
 vec4 mandelbrot(vec2 center, float zoom, float t) {
   vec2 p = vUv - 0.5;
@@ -72,13 +64,19 @@ vec4 mandelbrot(vec2 center, float zoom, float t) {
     }
   }
   //apply coloring as function of t:
-  float m = mod(t,300.);
-  if (m > 200.) {
+  float m = mod(t,390.);
+  if (m < 100.) {
     return colmod1(iter, t);
-  } else if(m > 100.) {
+  } else if (m < 130.) {
+    return mix(colmod1(iter, t), colmod2(iter, t), (m-100.)/30.);
+  } else if (m < 230.) {
     return colmod2(iter, t);
-  } else {
+  } else if (m < 260.) {
+    return mix(colmod2(iter, t), colmod3(iter, t), (m-230.)/30.);
+  } else if (m < 360.) {
     return colmod3(iter, t);
+  } else {
+    return mix(colmod3(iter, t), colmod1(iter, t), (m-360.)/30.);
   }
 }
 
