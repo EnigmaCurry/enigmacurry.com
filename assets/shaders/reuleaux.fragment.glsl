a264f13c51c462d92f1247a6528d7d4a58a0b7f0
Ryan McGuire <ryan@enigmacurry.com>
2019-02-17 21:44:23 -0500

    equilateral barycenter

--- assets/shaders/reuleaux.fragment.glsl
+++ assets/shaders/reuleaux.fragment.glsl
@@ -18,23 +18,27 @@ vec2 rotateVec2(in vec2 v, in vec2 axis, in float angle) {
   return vrot + axis;
 }
 
-vec3 reuleauxTriangle(in vec2 _p, in vec2 _center, in float r) {
-  vec2 top = vec2(_center.x, _center.y + (sqrt(3.)/2.) * r);
+vec3 reuleauxTriangle(in vec2 _p, in vec2 _rotCenter, in float r) {
+  vec2 top = vec2(_rotCenter.x, _rotCenter.y + (sqrt(3.)/2.) * r);
   vec2 left = vec2(top.x-(r/2.), top.y-1. * (sqrt(3.)/2.) * r);
   vec2 right = vec2(top.x+r/2., top.y-1. * (sqrt(3.)/2.) * r);
+  vec2 center = vec2(_rotCenter.x, _rotCenter.y + 0.333 * distance(_rotCenter, top));
   /// Rotate triangle
   float angle = sin(iTime / (8. + smoothstep(-1., 1., sin(iTime)))) * 4.;
-  top = rotateVec2(top, _center, angle);
-  left = rotateVec2(left, _center, angle);
-  right = rotateVec2(right, _center, angle);
+  top = rotateVec2(top, _rotCenter, angle);
+  left = rotateVec2(left, _rotCenter, angle);
+  right = rotateVec2(right, _rotCenter, angle);
+  center = rotateVec2(center, _rotCenter, angle);
   /// Union of three circles:
   float c1 = circle(_p, top, r);
   float c2 = circle(_p, left, r);
   float c3 = circle(_p, right, r);
-  float d = distance(_p, _center);
+  float drot = distance(_p, _rotCenter);
   float dtop = distance(_p, top);
   float dleft = distance(_p, left);
   float dright = distance(_p, right);
+  float dcenter = distance(_p, center);
+  float dthreshold = 0.008;
   vec3 color = vec3(0.);
   if ( c1 > 0. && c2 > 0. && c3 > 0.){
     float c = atan(cos((_p.x/_p.y) * abs(sin(iTime/14.)) * 13. + 3.) * 15.);
@@ -42,16 +46,21 @@ vec3 reuleauxTriangle(in vec2 _p, in vec2 _center, in float r) {
                       vec3(smoothstep(-1.48, 1.48, atan(cos(iTime/8.)*22.*sin(iTime/2.)))),
                       vec3(1.0 - c, c, _p.x * c),
                       vec3(smoothstep(-PI/4., PI/4.,atan(sin(iTime/3.))))
-                        - distance(_p, _center)
+                        - distance(_p, _rotCenter)
                       );
-    if (d < 0.005) {
-      color = vec3(smoothstep(0.,.01,d * smoothstep(-1.,1., sin(iTime/2.))),
-                   smoothstep(0.,.01,d * smoothstep(-1.,1., sin(iTime/4.))),
-                   smoothstep(0.,.01,d * smoothstep(-1.,1., sin(iTime/8.)))
+    if (drot < dthreshold) {
+      color = vec3(smoothstep(0.,.01,drot * smoothstep(-1.,1., sin(iTime/2.))),
+                   smoothstep(0.,.01,drot * smoothstep(-1.,1., sin(iTime/4.))),
+                   smoothstep(0.,.01,drot * smoothstep(-1.,1., sin(iTime/8.)))
                    );
-    } else if (dtop < 0.005 || dleft < 0.005 || dright < 0.005) {
+    } else if (dtop < dthreshold || dleft < dthreshold || dright < dthreshold) {
       color = vec3(1.);
-    } 
+    } else if (dcenter < dthreshold) {
+      color = vec3(smoothstep(0.,.01,dcenter * smoothstep(-1.,1., cos(iTime/2.))),
+                   smoothstep(0.,.01,dcenter * smoothstep(-1.,1., cos(iTime/4.))),
+                   smoothstep(0.,.01,dcenter * smoothstep(-1.,1., cos(iTime/8.)))
+                   );
+    }
   }
 
   return color;
