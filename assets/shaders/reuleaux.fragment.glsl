18ef16a5f98b9281b0c631ef4dc1bf77877745e9
Ryan McGuire <ryan@enigmacurry.com>
2019-02-18 00:34:49 -0500

    triangle lines

--- assets/shaders/reuleaux.fragment.glsl
+++ assets/shaders/reuleaux.fragment.glsl
@@ -11,6 +11,12 @@ float circle(in vec2 _p, in vec2 _center, in float _radius) {
   return step(distance(_p, _center), _radius);
 }
 
+float line(in vec2 p, in vec2 p1, in vec2 p2, float width) {
+  vec2 d0 = p2 - p1;
+  vec2 d1 = p - p1;
+  return length(d1 - d0 * clamp(dot(d0, d1) / dot(d0, d0), 0., 1.)) * (1./width*8.);
+}
+
 vec2 rotateVec2(in vec2 v, in vec2 axis, in float angle) {
   vec2 vprime = v - axis;
   vec2 vrot = vec2(vprime.x * cos(angle) - vprime.y * sin(angle),
@@ -39,6 +45,9 @@ vec3 reuleauxTriangle(in vec2 _p, in vec2 _rotCenter, in float r) {
   float dright = distance(_p, right);
   float dcenter = distance(_p, center);
   float dthreshold = 0.008;
+  float dline1 = line(_p, top, left, 1.);
+  float dline2 = line(_p, left, right, 1.);
+  float dline3 = line(_p, right, top, 1.);
   vec3 color = vec3(0.);
   if ( c1 > 0. && c2 > 0. && c3 > 0.){
     float c = atan(cos((_p.x/_p.y) * abs(sin(iTime/14.)) * 13. + 3.) * 15.);
@@ -60,6 +69,8 @@ vec3 reuleauxTriangle(in vec2 _p, in vec2 _rotCenter, in float r) {
                    smoothstep(0.,.01,dcenter * smoothstep(-1.,1., cos(iTime/4.))),
                    smoothstep(0.,.01,dcenter * smoothstep(-1.,1., cos(iTime/8.)))
                    );
+    } else if (dline1 < dthreshold || dline2 < dthreshold || dline3 < dthreshold) {
+      color = vec3(0.1);
     }
   }
 
@@ -97,8 +108,8 @@ void main(void)
   float radius = width / 2.;
   float tintAmount = smoothstep(-4.,8.,sin(iTime/2.));
   vec3 color = tint(vec3(0.,1.,0.), reuleauxTriangle(p, vec2(0., 0.), radius), tintAmount);
-  color += tint(vec3(75./255.,0.,130./255.), reuleauxTriangle(p, vec2(radius, 0.), radius), tintAmount);
-  color += tint(vec3(1.,165./255.,0.), reuleauxTriangle(p, vec2(-radius, 0.), radius), tintAmount);
+  color += tint(vec3(0.294,0.,0.50980), reuleauxTriangle(p, vec2(radius, 0.), radius), tintAmount);
+  color += tint(vec3(1.,0.64706,0.), reuleauxTriangle(p, vec2(-radius, 0.), radius), tintAmount);
   color += tint(vec3(1.,0.,0.), reuleauxTriangle(p,
                             vec2(-radius/2., (sqrt(3.)/2.) * radius),
                                                  radius), tintAmount);
@@ -108,7 +119,7 @@ void main(void)
   color += tint(vec3(1.,1.,0.), reuleauxTriangle(p,
                             vec2(-radius/2., 0. - (sqrt(3.)/2.) * radius),
                                                  radius), tintAmount);
-  color += tint(vec3(238./255., 130./255., 238./255.), reuleauxTriangle(p,
+  color += tint(vec3(0.93333, 0.50980, 0.93333), reuleauxTriangle(p,
                             vec2(radius/2., 0. - (sqrt(3.)/2.) * radius),
                                                                         radius), tintAmount);
   gl_FragColor = bgScene(color, p);
