23fa4f4d6879356ac9ea80df8ecccb83482f4f72
Ryan McGuire <ryan@enigmacurry.com>
2019-02-19 10:26:43 -0500

    sine wave

--- assets/shaders/graph.fragment.glsl
+++ assets/shaders/graph.fragment.glsl
@@ -2,25 +2,50 @@ uniform float iTime;
 uniform vec2 iResolution;
 varying vec2 vUv;
 
-float line(in vec2 p, in vec2 p1, in vec2 p2, float width) {
+#define iZoom 0.5
+#define iField 20.
+#define iCenter vec2(0., 0.)
+#define PI 3.14159
+
+float line(in vec2 p, in vec2 p1, in vec2 p2, in float width) {
   vec2 d0 = p2 - p1;
   vec2 d1 = p - p1;
   return length(d1 - d0 * clamp(dot(d0, d1) / dot(d0, d0), 0., 1.)) * (1./width*8.);
 }
 
-vec4 axes(vec2 p) {
-  vec3 xaxis = vec3(1./line(p, vec2(-1.,0.), vec2(1.,0.), 0.01));
-  vec3 yaxis = vec3(1./line(p, vec2(0.,-1.), vec2(0.,1.), 0.01));
-  vec3 buf = xaxis;
-  if (buf.r < 1.) {
-    buf += yaxis;
+vec3 layer(in vec3 buf, in vec3 color) {
+  if (color.r > 0.1 || color.g > 0.1 || color.b > 0.1) {
+    return color;
+  } else {
+    return buf;
   }
-  return vec4(buf, 1.);
+}
+
+vec3 axes(in vec2 p, in float length) {
+  vec3 xaxis = vec3(1./line(p, vec2(-length,0.), vec2(length,0.), 0.001));
+  vec3 yaxis = vec3(1./line(p, vec2(0.,-length), vec2(0.,length), 0.001));
+  vec3 buf = layer(vec3(0.), xaxis);
+  buf = layer(buf, yaxis);
+  return buf;
+}
+
+float func1(float x) {
+  float fx = sin(x * 2.);
+  return fx;
+}
+
+float plot(in vec2 p) {
+  const float e = 0.001;
+  p.y -= func1(p.x);
+  float g = (func1(p.x + e) - func1(p.x - e)) / (PI * e);
+  return abs(p.y * cos(atan(g)));
 }
 
 void main(void)
 {
-  vec2 p = vUv - 0.5;
+  vec2 p = (vUv - 0.5 + iCenter) / iZoom;
   p.x *= iResolution.x/iResolution.y;
-  gl_FragColor = axes(p);
+  vec3 buf = axes(p, 300.);
+  buf += smoothstep(0.011, 0.0, plot(p));
+  gl_FragColor = vec4(buf, 1.);
 }
