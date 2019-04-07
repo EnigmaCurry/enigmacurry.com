91944eca62bd83b3a1ec5a8a8f35caeaa709cef4
Ryan McGuire <ryan@enigmacurry.com>
2019-04-07 19:27:03 -0400

    Try to outfox the browsers rules about autoplay+sound.

--- components/scenes/video-test.vue
+++ components/scenes/video-test.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <video autoplay muted loop height="100%" width="100%">
+    <video autoplay muted loop height="100%" width="100%" ref="video" >
     <source src="https://spee.ch/@giuseppe:5/buck.mp4" type="video/mp4">
     Your browser does not support video
   </video>
@@ -8,4 +8,16 @@
 </template>
 
 <script>
+export default {
+  mounted() {
+    let video = this.$refs.video
+    setTimeout(() => {
+      video.muted = false
+    }, 1000)
+    setTimeout(() => {
+      video.play()
+    }, 2000)
+
+  }
+}
 </script>
