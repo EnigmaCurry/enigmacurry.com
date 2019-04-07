09fef3946b24486b57bb6f76f720e339ff4dd8d1
Ryan McGuire <ryan@enigmacurry.com>
2019-04-07 19:14:23 -0400

    mute video to force autoplay?

--- components/scenes/video-test.vue
+++ components/scenes/video-test.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <video autoplay loop height="100%" width="100%">
+    <video autoplay muted loop height="100%" width="100%">
     <source src="https://spee.ch/@giuseppe:5/buck.mp4" type="video/mp4">
     Your browser does not support video
   </video>
