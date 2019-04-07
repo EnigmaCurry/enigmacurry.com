28742926fa64917d4ff1ca66f47d92a7233c96b4
Ryan McGuire <ryan@enigmacurry.com>
2019-04-07 18:53:56 -0400

    Video test

---
+++ pages/video-test.vue
@@ -0,0 +1,14 @@
+<template>
+  <video-test-scene />
+</template>
+
+<script>
+import ScrollbarHide from '~/components/ScrollbarHide.vue'
+import VideoTestScene from '~/components/scenes/video-test.vue'
+
+export default {
+  layout: 'default',
+  mixins: [ScrollbarHide],
+  components: {VideoTestScene},
+}
+</script>
