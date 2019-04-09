b85a6f9b27ccf48589e03b1dc6beee877687c09f
Ryan McGuire <ryan@enigmacurry.com>
2019-04-09 02:09:07 -0400

    Video player static positioning

--- components/scenes/video-player.vue
+++ components/scenes/video-player.vue
@@ -3,12 +3,25 @@
     <g-camera name="main" ref="camera" orthographic/>
     <g-grid :divisions="20" :size="20" v-if="showGrid"/>
     <animation :fn="animate" />
-    <video autoplay loop height="100%" width="100%" ref="video" >
-    <source :src="video" type="video/mp4">
-    Your browser does not support video
-    </video>
+    <div class="video-player">
+      <video autoplay loop height="100%" width="100%" ref="video" >
+      <source :src="video" type="video/mp4">
+      Your browser does not support video
+      </video>
+    </div>
   </g-scene>
 </template>
+
+<style scoped>
+.video-player {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+</style>
+
 
 <script>
 import * as Three from 'three'
