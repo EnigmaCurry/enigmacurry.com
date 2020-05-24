b223d5b7c8844a69e0b9b7bb3efc48b95ca46445
EnigmaCurry <ryan@enigmacurry.com>
2020-05-24 18:31:46 -0400

    MYRCURY videos

---
+++ pages/video/myrcury-turn-in.vue
@@ -0,0 +1,21 @@
+<template>
+<video-player
+  video="https://cdn.rymcg.tech/Video/MYRCURY/MYRCURY-turn-in.mp4"/>
+</template>
+
+<style scoped>
+video {
+  pointer-events: auto;
+}
+</style>
+
+<script>
+import ScrollbarHide from '~/components/ScrollbarHide.vue'
+import VideoPlayer from '~/components/scenes/video-player.vue'
+
+export default {
+  layout: 'renderer',
+  mixins: [ScrollbarHide],
+  components: {VideoPlayer},
+}
+</script>
