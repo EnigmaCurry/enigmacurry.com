ffff2e7db0bf20cfa0d972017cb8f2cbbab5e010
EnigmaCurry <ryan@enigmacurry.com>
2020-05-25 16:38:15 -0400

    playback button

--- components/scenes/video-player.vue
+++ components/scenes/video-player.vue
@@ -4,7 +4,8 @@
     <g-grid :divisions="20" :size="20" v-if="showGrid"/>
     <animation :fn="animate" />
     <div class="video-player">
-      <video autoplay loop height="100%" width="100%" ref="video" >
+      <div v-if="playbackPrevented" id="clickme" class="overlay"><button class="button play"></button></div>
+      <video loop height="100%" width="100%" ref="video" >
       <source :src="video" type="video/mp4">
       Your browser does not support video
       </video>
@@ -13,12 +14,33 @@
 </template>
 
 <style scoped>
+.overlay{
+    background: #333;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.2);
+}
 .video-player {
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+}
+.button.play {
+  box-sizing: border-box;
+  width: 74px;
+  height: 74px;
+  border-style: solid;
+  border-width: 37px 0px 37px 74px;
+  border-color: transparent transparent transparent #ffffff;
 }
 </style>
 
@@ -33,12 +55,12 @@ export default {
   props: {
     backgroundAlpha: {type: Number, default: 0},    
     showGrid: {type: Boolean, default: false},
-    video: {type: String },
-    pauseMusic: {type: Boolean},
+    video: {type: String }
   },
   data() {
     return {
       scene: new Three.Scene(),
+      playbackPrevented: false
     }
   },
   methods: {
@@ -46,9 +68,18 @@ export default {
     },    
     onClick(e) {
       this.$refs.video.play()
+      this.playbackPrevented = false
     }
   },
-  mounted() {
+mounted() {
+    var promise = this.$refs.video.play()
+    if (promise !== undefined) {
+      promise.then(_ => {
+        this.playbackPrevented = false
+      }).catch(error => {
+        this.playbackPrevented = true
+      })
+    }
     document.addEventListener('click', this.onClick)
     if (this.pauseMusic) {
       this.$bus.$emit('music-pause')
